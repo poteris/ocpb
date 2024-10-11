45409a9700e1aaac18e92902e29ef7c320891a98
robertvava <vavarobert10@gmail.com>
2024-10-11 16:47:35 +0100

    migrated

--- app/api/chat/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST(request: Request) {
-  const { message } = await request.json();
-  // Process the message and generate a response
-  const response = 'Simulated response to: ' + message;
-  return NextResponse.json({ response });
-}
--- src/components/ui/index.ts
+++
@@ -1,3 +0,0 @@
-export { Button } from "./Button";
-export { InfoPopover } from "./Popover";
-export { Modal } from "./Modal";
