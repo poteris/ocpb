9ba76d5ba1d18205f2a18fa7625200af4831e101
Red-Cardigan <jethro.reeve@gmail.com>
2024-10-23 18:07:36 +0100

    Use local url in dev only

--- frontend/src/utils/supabaseQueries.ts
+++ frontend/src/utils/supabaseQueries.ts
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 import { PersonaInfo } from '@/context/ScenarioContext';
 
-const supabaseUrl = 'http://127.0.0.1:54321' || process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseUrl = process.env.NODE_ENV === 'development'
+  ? 'http://127.0.0.1:54321'
+  : process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
 
 if (!supabaseUrl || !supabaseAnonKey) {
