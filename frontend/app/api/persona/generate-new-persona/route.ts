1eeb619b8344f4d6fdf974db330e3401cdcef721
thelonehegelian <thelonehegelian@protonmail.com>
2025-01-22 19:23:55 +0000

    fix: import issue and remove unused request

--- frontend/app/api/persona/generate-new-persona/route.ts
+++ frontend/app/api/persona/generate-new-persona/route.ts
@@ -1,13 +1,13 @@
-import {generatedPersona as newPersona} from './newPersona'
+import { generatedPersona as newPersona } from './generateNewPersona'
 
-export async function GET(request: Request) {
-    
-    const persona = newPersona;
-    console.log(persona)
-    return new Response(JSON.stringify(persona), {
-        status: 200,
-        headers: {
-            'Content-Type': 'text/plain',
-        },
-    });
+export async function GET() {
+
+  const persona = newPersona;
+  console.log(persona)
+  return new Response(JSON.stringify(persona), {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/plain',
+    },
+  });
 }
