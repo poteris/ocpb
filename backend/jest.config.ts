634be281d99516da224fbe9de9afaa83ba8f4739
Dave Balderstone <dave@poteris.co.uk>
2025-01-23 21:34:40 +0000

    OCPB-27 Remove redundant jest config

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -6,20 +6,3 @@ const config: Config = {
 };
 
 export default config;
-
-
-/*import { createDefaultPreset, JestConfigWithTsJest } from 'ts-jest'
-
-const config: JestConfigWithTsJest = {
-  // A preset that is used as a base for Jest's configuration
-  // npm install -D 'ts-jest'
-
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
-  },
-  preset: 'ts-jest',
-  roots: ['<rootDir>/__test__'],
-  verbose: true,
-};*/
