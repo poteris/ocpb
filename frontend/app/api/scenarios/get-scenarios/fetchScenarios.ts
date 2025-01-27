be99b9678998aec03999ab05490e32fb6d9654c6
thelonehegelian <thelonehegelian@protonmail.com>
2025-01-27 13:32:36 +0000

    refactor: move get scenario db call to next server
    - refactored the db call as well

---
+++ frontend/app/api/scenarios/get-scenarios/fetchScenarios.ts
@@ -0,0 +1,36 @@
+import { supabase } from '@/lib/supabaseClient';
+import  { TrainingScenario } from '@/types/scenarios'
+ async function getScenarios(): Promise<TrainingScenario[]> {
+    const { data, error } = await supabase
+      .from('scenarios')
+      .select(`
+        id,
+        title,
+        description,
+        context,
+        scenario_objectives (objective)
+      `);
+  
+  if (error) {
+      console.error('Error fetching scenarios:', {
+          message: error.message,
+          details: error.details,
+          hint: error.hint,
+          code: error.code
+      });
+      return [];
+  }
+  
+  
+    // Combine scenarios with their objectives
+    return data.map(data => ({
+     id: data.id,
+      title: data.title,
+      description: data.description,
+      context: data.context,
+      objectives: data.scenario_objectives.map(obj => obj.objective) || [],
+  
+    }));
+  }
+
+  export { getScenarios };
