cf92a589561dbe41aa39fe458bc176068b4b16aa
thelonehegelian <thelonehegelian@protonmail.com>
2025-01-29 14:06:04 +0000

    fix: ocpb-58-add id to the new persona and validate with zod

--- frontend/app/api/persona/generate-new-persona/route.ts
+++ frontend/app/api/persona/generate-new-persona/route.ts
@@ -1,11 +1,29 @@
 import { generateNewPersona as newPersona } from './generateNewPersona';
+import { z } from "zod";
+
+ const personaSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  segment: z.string(),
+  age: z.number(),
+  gender: z.string(),
+  family_status: z.string(),
+  uk_party_affiliation: z.string(),
+  workplace: z.string(),
+  job: z.string(),
+  busyness_level: z.string(),
+  major_issues_in_workplace: z.string(),
+  personality_traits: z.string(),
+  emotional_conditions: z.string(),
+});
 
 export async function GET() {
   try {
     const persona = await newPersona();
     
+    personaSchema.parse(persona); // will throw an error if the persona does not match the schema
+    
     console.log(persona); 
-    // Return the persona as a JSON response
     return new Response(JSON.stringify(persona), {
       status: 200,
       headers: {
