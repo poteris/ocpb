2aaec388ce1d2750d695f982090fdb68e3ff8190
thelonehegelian <thelonehegelian@protonmail.com>
2025-01-30 10:30:53 +0000

    refactor: ocpb-67-update to use next server for sending message
    - use global state for scenario and persona where required
    - update Message type name
    - create api for sending messages
    - use uuid for id

---
+++ frontend/app/api/chat/send-user-message/route.ts
@@ -0,0 +1,59 @@
+import { NextRequest, NextResponse } from "next/server";
+import { supabase } from "@/lib/supabaseClient";
+import { v4 as uuid } from "uuid";
+import { z } from "zod";
+
+const userMessageResponseSchema = z.object({
+  id: z.string(),
+  text: z.string(),
+  sender: z.string(),
+});
+
+const sendUserMessageRequestSchema = z.object({
+  conversationId: z.string(),
+  content: z.string(),
+  scenario_id: z.string(),
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    const parsedBody = sendUserMessageRequestSchema.parse(body);
+
+    if (process.env.NODE_ENV !== "production") {
+      return NextResponse.json(
+        { id: uuid(), text: "Hello", sender: "bot" },
+        { status: 200 }
+      );
+    }
+
+    const { data, error } = await supabase.functions.invoke("assistant", {
+      body: JSON.stringify({
+        action: "sendMessage",
+        ...parsedBody,
+      }),
+    });
+
+
+    if (error || !data) {
+      console.error("Error invoking assistant function:", error);
+      return NextResponse.json(
+        { error: "Failed to get a message from LLM" },
+        { status: 500 }
+      );
+    }
+
+    const llmMessage = { id: uuid(), text: data.result.content, sender: "bot" };
+
+    const parsedResponse = userMessageResponseSchema.parse(llmMessage);
+
+    return NextResponse.json(parsedResponse, { status: 200 });
+  } catch (error) {
+    console.error("Error creating new chat:", error);
+    return NextResponse.json(
+      { error: "Failed to create new chat" },
+      { status: 500 }
+    );
+  }
+}
