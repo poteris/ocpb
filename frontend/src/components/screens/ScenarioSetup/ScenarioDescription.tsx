589e17993ebbab663ed7f79e6dafd6c5ffc63b20
thelonehegelian <thelonehegelian@protonmail.com>
2025-02-03 14:27:07 +0000

    fix: missing persona in initiate chat
    - get scenario and persona from the global state

--- frontend/src/components/screens/ScenarioSetup/ScenarioDescription.tsx
+++ frontend/src/components/screens/ScenarioSetup/ScenarioDescription.tsx
@@ -1,34 +1,32 @@
-
-import ReactMarkdown from 'react-markdown';
-import { markdownStyles } from '@/utils/markdownStyles';
-import {TrainingScenario} from '@/types/scenarios';
+import ReactMarkdown from "react-markdown";
+import { markdownStyles } from "@/utils/markdownStyles";
+import { TrainingScenario } from "@/types/scenarios";
 interface ScenarioDescriptionProps {
-    scenario: TrainingScenario;
+  scenario: TrainingScenario;
 }
 
-const  ScenarioDescription: React.FC<ScenarioDescriptionProps> = ({ scenario }) => {
-    return (
-        <div>
-            <section className="mb-12">
-                <h2 className="text-3xl font-semibold mb-6 text-gray-900 dark:text-gray-100">
-                    Scenario Description
-                </h2>
-                <p className="text-lg text-gray-700 dark:text-gray-300">{scenario?.description}</p>
-            </section>
+const ScenarioDescription: React.FC<ScenarioDescriptionProps> = ({ scenario }) => {
+  return (
+    <div>
+      <section className="mb-12">
+        <h2 className="text-3xl font-semibold mb-6 text-gray-900 dark:text-gray-100">
+          Scenario Description
+        </h2>
+        <p className="text-lg text-gray-700 dark:text-gray-300">{scenario?.description}</p>
+      </section>
 
-            <section>
-                <h2 className="text-3xl font-semibold mb-6 text-gray-900 dark:text-gray-100">Objectives</h2>
-                <div className="text-lg text-gray-700 dark:text-gray-300 space-y-4">
-                    {scenario?.objectives.map((objective, index) => (
-                        <div key={index} className="mb-4">
-                            <ReactMarkdown components={markdownStyles}>{objective}</ReactMarkdown>
-                        </div>
-                    ))}
-                </div>
-            </section>
+      <section>
+        <h2 className="text-3xl font-semibold mb-6 text-gray-900 dark:text-gray-100">Objectives</h2>
+        <div className="text-lg text-gray-700 dark:text-gray-300 space-y-4">
+          {scenario?.objectives.map((objective, index) => (
+            <div key={index} className="mb-4">
+              <ReactMarkdown components={markdownStyles}>{objective}</ReactMarkdown>
+            </div>
+          ))}
         </div>
-    );
+      </section>
+    </div>
+  );
 };
-
 
 export default ScenarioDescription;
