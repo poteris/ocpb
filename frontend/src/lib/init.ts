d440c49987f0cc3be2dc82753231b2c9880dfaea
thelonehegelian <thelonehegelian@protonmail.com>
2025-02-03 16:46:18 +0000

    refactor: move supabase and openai initialisation to init

--- frontend/src/lib/init.ts
+++ frontend/src/lib/init.ts
@@ -1,10 +1,19 @@
 import { createClient } from "@supabase/supabase-js";
+import { OpenAI } from "openai";
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL or env.NEXT_PUBLIC_SUPABASE_ANON_KEY");
+  throw new Error("Missing env.SUPABASE_URL or env.SUPABASE_ANON_KEY");
 }
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+
+const openaiClient = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
+export { openaiClient };
