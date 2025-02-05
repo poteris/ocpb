55ed04c7518eca620636ce8c92809a624f8165b5
Dave Balderstone <dave@poteris.co.uk>
2025-02-05 09:11:51 +0000

    OCPB-26 Initial playwright setup, with a couple of tests

---
+++ frontend/e2e/smoke-tests.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const baseUrl = process.env.E2E_TEST_BASE_URL;
+
+test('Landing page loads and the call to action button starts the user journey for member recruitment', async ({ page }) => {
+  await page.goto(`${baseUrl}`);
+  await expect(page.getByRole('button', { name: 'Start Scenario' })).toBeVisible();
+  await page.getByRole('button', { name: 'Start Scenario' }).click();
+  // Assert that the URL is the expected one
+  await expect(page).toHaveURL(`${baseUrl}/scenario-setup?scenarioId=member-recruitment`);
+});
+
+test('Route back to landing page from scenario setup page', async ({ page }) => {
+  await page.goto(`${baseUrl}/scenario-setup?scenarioId=member-recruitment`);
+  await expect(page.getByRole('button', { name: 'Back to Scenarios' })).toBeVisible();
+  await page.getByRole('button', { name: 'Back to Scenarios' }).click();
+  // Assert that the URL is the expected one
+  await expect(page).toHaveURL(`${baseUrl}`);
+});
