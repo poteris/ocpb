b5ad935cdc0e2181373c367b1453bd1c1ba9ab9d
thelonehegelian <thelonehegelian@protonmail.com>
2025-02-11 22:49:54 +0000

    refactor: ocpb-98 update scripts
    - update backend and frontend scripts
    - we can run all the required commands from root
    - remove concurrently as not needed
    - update .env example file
    - load env vars from the root folder
    - deps: update dotenv

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env' });
+
+const nextConfig = {
+    env: {
+        SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    },
+};
+
+export default nextConfig;
