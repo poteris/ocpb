da36bd2a03f2b5cc8c1b00191c83d227d02353f7
Dave Balderstone <dave@poteris.co.uk>
2025-02-12 16:29:17 +0000

    OCPB-26. Update slack action to 2.0 - refine messaging

--- frontend/e2e/smoke-tests.spec.ts
+++ frontend/e2e/smoke-tests.spec.ts
@@ -7,11 +7,12 @@ dotenv.config();
 const baseUrl = process.env.E2E_TEST_BASE_URL;
 
 test('Landing page loads and the call to action button starts the user journey for member recruitment', async ({ page }) => {
+  test.fail(); // for debugging
   await page.goto(`${baseUrl}`);
   await expect(page.getByRole('button', { name: 'Start Scenario' })).toBeVisible();
   await page.getByRole('button', { name: 'Start Scenario' }).click();
   // Assert that the URL is the expected one
-  await expect(page).toHaveURL(`${baseUrl}/scenario-setup?scenarioId=member-recruitments`);
+  await expect(page).toHaveURL(`${baseUrl}/scenario-setup?scenarioId=member-recruitment`);
 });
 
 test('Route back to landing page from scenario setup page', async ({ page }) => {
