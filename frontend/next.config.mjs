08086123cba94d50836485e8765224a570003b03
thelonehegelian <thelonehegelian@protonmail.com>
2025-02-13 15:35:16 +0000

    fix: env variable names

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
 
 const nextConfig = {
-    env: {
-        SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-        SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    },
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
 };
 
 export default nextConfig;
