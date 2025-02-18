cccd867c63a4d85445fe4fd3dae0f8c6718f5aad
thelonehegelian <thelonehegelian@protonmail.com>
2025-02-18 11:47:25 +0000

    feat: create and add global navbar

---
+++ frontend/src/components/navigation/NavBar.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+
+const NavBar = () => {
+  return (
+    <nav className="h-[85px] bg-primary w-full flex items-center justify-between px-4 md:px-8">
+      <div className="text-white text-[30px] font-regular ml-8">
+        Rep Coach
+      </div>
+      
+      <div className="relative h-12 w-12 md:h-16 md:w-16">
+        <Image
+          src="/images/pcs-logo.svg"
+          alt="Rep Coach Logo"
+          fill
+          className="object-contain"
+          priority
+        />
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar 
