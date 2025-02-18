586859925b4d47123a425311b8dac58c6f376708
thelonehegelian <thelonehegelian@protonmail.com>
2025-02-18 12:24:48 +0000

    fix: ocpb-108 remove duplicate Input type
    - was conflicting with Shadcn

--- frontend/src/components/ui/index.ts
+++ frontend/src/components/ui/index.ts
@@ -1,6 +1,5 @@
 export { InfoPopover } from "./Popover";
 export { Modal } from "./Modal";
-export { Input } from "./Input";
 export { Loader } from "./Loader";
 export { Skeleton } from "./Skeleton";
 export { Tabs, TabsList, TabsTrigger, TabsContent } from "./Tabs";
