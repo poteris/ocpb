d412f7127684088adf69f132886928a5db01d785
thelonehegelian <thelonehegelian@protonmail.com>
2025-02-20 13:27:44 +0000

    ui: ocpb-92 update welcome message to have space between paragraphs

--- frontend/src/components/screens/Welcome/WelcomeMessage.tsx
+++ frontend/src/components/screens/Welcome/WelcomeMessage.tsx
@@ -1,23 +1,26 @@
 import { Card, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 
-
 const WelcomeMessage = () => {
-    return (
-    
-    <Card className="border-none rounded-[20px] bg-card-alt p-4 md:p-8 ">
+  return (
+    <Card className="border-none rounded-[20px] bg-card-alt p-4 md:p-8">
       <CardHeader>
         <CardTitle className="text-2xl font-light mb-2">Welcome</CardTitle>
         <CardDescription className="leading-relaxed space-y-4">
-        Welcome to RepCoach! This is a safe space where you can practice your conversation as a trade union representative, in different scenarios.
-For each scenario, you will be given objectives to achieve and details of the persona that you will be chatting with. When you have finished chatting, you will be presented with feedback that evaluates your performance against the objectives.
-<br />
-<br />
-Have fun!
+          <p>
+            Welcome to RepCoach! This is a safe space where you can practice your 
+            conversation as a trade union representative, in different scenarios.
+          </p>
+          <p>
+            For each scenario, you will be given objectives to achieve and details 
+            of the persona that you will be chatting with. When you have finished 
+            chatting, you will be presented with feedback that evaluates your 
+            performance against the objectives.
+          </p>
+          <p>Have fun!</p>
         </CardDescription>
       </CardHeader>
     </Card>
-    );
-  };
+  );
+};
 
-
-  export default WelcomeMessage;
+export default WelcomeMessage;
