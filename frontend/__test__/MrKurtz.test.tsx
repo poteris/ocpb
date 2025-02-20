d481c7200bb7da714d221ac2b6c2251290969c72
thelonehegelian <thelonehegelian@protonmail.com>
2025-02-20 10:04:54 +0000

    ocpb-126 remove sample tests

--- frontend/__test__/MrKurtz.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import { MrKurtz } from "./MrKurtz";
-import "@testing-library/jest-dom";
-
-test("renders Mr Kurtz!", () => {
-  render(<MrKurtz />);
-  expect(screen.getByText("Mr Kurtz - He Dead!")).toBeInTheDocument();
-});
