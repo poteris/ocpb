65ae29b0732534d9ea332af99c45a055d87e3852
thelonehegelian <thelonehegelian@protonmail.com>
2025-02-25 17:02:44 +0000

    fix: ocpb-124 add use client

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
