d26c22ddaf1b60b9df826b08d7bcca8a2ddadbb7
umr <24369269+thelonehegelian@users.noreply.github.com>
2026-07-15 11:09:18 +0100

    [LEADERBOARD] Fix seed and start data for fresh db reset

--- backend/supabase/seed.sql
+++ backend/supabase/seed.sql
@@ -158,13 +158,6 @@ SELECT pg_catalog.setval('"auth"."refresh_tokens_id_seq"', 1, false);
 
 
 --
--- Name: key_key_id_seq; Type: SEQUENCE SET; Schema: pgsodium; Owner: supabase_admin
---
-
-SELECT pg_catalog.setval('"pgsodium"."key_key_id_seq"', 1, false);
-
-
---
 -- Name: hooks_id_seq; Type: SEQUENCE SET; Schema: supabase_functions; Owner: supabase_functions_admin
 --
 
